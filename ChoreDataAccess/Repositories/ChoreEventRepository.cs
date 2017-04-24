e364ad540dc32d8468be0bc41c619266bfb4daa0
Alex Elia <alex@coderpages.com>
2017-04-23 18:26:08 -0700

    Starting implementation of the ChoreEvent business API.

--- ChoreDataAccess/Repositories/ChoreEventRepository.cs
+++ ChoreDataAccess/Repositories/ChoreEventRepository.cs
@@ -11,7 +11,36 @@ namespace ChoreDataAccess.Repositories
     {
         public ChoreEvent Get(int id)
         {
-            throw new NotImplementedException();
+            var results = new ChoreEvent();
+
+            string sqlString = "SELECT Id, ChoreId, EventDate " +
+                               "FROM ChoreEvent " +
+                               "WHERE Id = @Id";
+
+            using (var connection = new SqlConnection(@"Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=True;"))
+            {
+                connection.Open();
+                using (var comm = new SqlCommand(sqlString, connection))
+                {
+                    comm.Parameters.AddWithValue("@Id", id);
+
+                    SqlDataReader reader = comm.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        ChoreEvent chore = new ChoreEvent();
+                        var i = -1;
+                        chore.Id = (int?)reader[++i];
+                        chore.Chore = new Chore();
+                        chore.Chore.Id = (int?)reader[++i];
+                        chore.Completed = (DateTime?)reader[++i];
+                        results = chore;
+                    }
+
+                    reader.Close();
+                }
+            }
+
+            return results;
         }
 
         public IEnumerable<ChoreEvent> GetList()
@@ -47,9 +76,22 @@ namespace ChoreDataAccess.Repositories
         
         public ChoreEvent Save(ChoreEvent entity)
         {
-            throw new NotImplementedException();
-            // TODO
-            // return entity;
+            var output = new ChoreEvent();
+            var sqlString = "IF (NOT EXISTS(SELECT Id, ChoreId, EventDate " +
+                               "FROM ChoreEvent " +
+                               "WHERE Id = @Id)) " +
+                               "BEGIN " + 
+                               "INSERT INTO ChoreEvent(ChoreId, EventDate) " +
+                               "VALUES(@ChoreId, @EventDate) " +
+                               "END " +
+                               "ELSE " +
+                               "BEGIN " +
+                               "UPDATE ChoreEvent " +
+                               "SET ChoreId = @ChoreId, EventDate = @EventDate " +
+                               "WHERE Id = @Id " +
+                               "END ";
+
+            return output;
         }
     }
 }
---
+++ ChoreServices/Interface/IChoreEventService.cs
@@ -0,0 +1,14 @@
+﻿using ChoreData;
+using System.Collections.Generic;
+
+namespace ChoreServices.Interface
+{
+    public interface IChoreEventService
+    {
+        ChoreEvent Save(ChoreEvent entity);
+
+        IEnumerable<ChoreEvent> GetList();
+
+        ChoreEvent GetRandomChore();
+    }
+}
