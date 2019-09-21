61ad59722a7d165cc15a9b4be1f0e72f3fe66c3f
BoredTweak <alex@boredtweak.com>
2019-09-21 08:51:38 -0700

    Adding sample data to sql Db script.

--- ChoreHelperDbCreation.sql
+++ ChoreHelperDbCreation.sql
@@ -1,3 +1,4 @@
+-- Setup schema
 IF db_id('ChoreHelper') IS NULL
 	CREATE DATABASE ChoreHelper;
 
@@ -12,3 +13,36 @@ CREATE TABLE ChoreHelper.dbo.ChoreEvent(
 	EventDate DateTime,
 	FOREIGN KEY (ChoreId) REFERENCES ChoreHelper.dbo.Chore(Id)
 	);
+
+-- Add sample data
+INSERT INTO [ChoreHelper].[dbo].[Chore] (Chore, FrequencyDays)
+VALUES 
+	('Dishes', 1),
+	('Vacuum', 7),
+	('Shampoo Carpet', 30),
+	('Clean Bath', 14),
+	('Laundry', 3),
+	('Clean Toilet(s)', 7),
+	('Windows', 21),
+	('Car(s)', 28),
+	('Baby Laundry', 14),
+	('Kitchen Counters', 2),
+	('Guest Bathroom', 7),
+	('Master Bathroom', 7),
+	('Clean Bedding', 7),
+	('Take Out Trash', 2),
+	('Power Wash', 365),
+	('Mow Lawn', 7),
+	('Mop Floors', 14),
+	('Sweep Floors', 3),
+	('Clean Fridge', 14),
+	('Pick Up Garage', 90),
+	('Pick Up Dog Poop', 7),
+	('Dust', 7);
+GO
+
+INSERT INTO [ChoreHelper].[dbo].[Contributor] (ContributorName)
+VALUES 
+	('Aimee'),
+	('Alex');
+GO
